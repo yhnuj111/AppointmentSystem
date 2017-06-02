540509ff9d2d3e7005db4da503e66fdcebe69aa0
yhnuj111 <xuz10@miamioh.edu>
2017-06-02 14:51:36 -0400

    Appointment_system

---
+++ src/RegisterPage.java
@@ -0,0 +1,170 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.awt.event.ActionEvent;
+
+public class RegisterPage extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField firstname_field;
+	private JTextField lastname_field;
+	private JTextField age;
+	private JTextField contactnum;
+	private JTextField username_field;
+	private JTextField password_field;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RegisterPage frame = new RegisterPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RegisterPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		firstname_field = new JTextField();
+		firstname_field.setBounds(150, 6, 130, 37);
+		contentPane.add(firstname_field);
+		firstname_field.setColumns(10);
+
+		JLabel lblFirstName = new JLabel("First Name");
+		lblFirstName.setBounds(29, 16, 75, 16);
+		contentPane.add(lblFirstName);
+
+		lastname_field = new JTextField();
+		lastname_field.setBounds(150, 55, 130, 37);
+		contentPane.add(lastname_field);
+		lastname_field.setColumns(10);
+
+		age = new JTextField();
+		age.setBounds(150, 104, 130, 37);
+		contentPane.add(age);
+		age.setColumns(10);
+
+		contactnum = new JTextField();
+		contactnum.setBounds(150, 153, 130, 37);
+		contentPane.add(contactnum);
+		contactnum.setColumns(10);
+
+		JLabel lblLastName = new JLabel("Last Name");
+		lblLastName.setBounds(29, 65, 75, 16);
+		contentPane.add(lblLastName);
+
+		JLabel lblAge = new JLabel("Age");
+		lblAge.setBounds(29, 114, 61, 16);
+		contentPane.add(lblAge);
+
+		JLabel lblContactNumber = new JLabel("Contact number");
+		lblContactNumber.setBounds(29, 163, 140, 16);
+		contentPane.add(lblContactNumber);
+
+		JLabel lblRegisterAs = new JLabel("Register as");
+		lblRegisterAs.setBounds(319, 66, 90, 16);
+		contentPane.add(lblRegisterAs);
+
+		JRadioButton rdbtnPateint = new JRadioButton("Visitor");
+		rdbtnPateint.setBounds(303, 94, 141, 23);
+		contentPane.add(rdbtnPateint);
+
+		JRadioButton rdbtnDoctor = new JRadioButton("Institution");
+		rdbtnDoctor.setBounds(303, 129, 141, 23);
+		contentPane.add(rdbtnDoctor);
+
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(rdbtnPateint);
+		bg.add(rdbtnDoctor);
+
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnDoctor.isSelected()){
+					try{
+						Connection myConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Account", "root", "root");
+						String firstname1 = firstname_field.getText();
+						String lastname1 = lastname_field.getText();
+						int age1 = Integer.parseInt(age.getText());
+						int contact = Integer.parseInt(contactnum.getText());
+						String username1 = username_field.getText();
+						String password1 = password_field.getText();
+						String query = "insert into DoctorIdInfo (DoctorId,lastname,firstname,username,password,age,contactNumber) values(2,?,?,?,?,?,?)";
+						PreparedStatement statement = myConn.prepareStatement(query);
+						statement.setString(1, lastname1);
+						statement.setString(2, firstname1);
+						statement.setString(3, username1);
+						statement.setString(4, password1);
+						statement.setInt(5, age1);
+						statement.setInt(6, contact);
+						
+						int set = statement.executeUpdate();
+	                    if(set == 1){
+	                    	JOptionPane.showMessageDialog(null, "Account created!");
+	                    	UserLogin ui = new UserLogin();	                
+	                    	ui.setVisible(true);
+	                    	dispose();
+	                    	
+	                    }else{
+	                    	JOptionPane.showMessageDialog(null, "Please enter valid information!");
+	                    }
+					}catch(Exception exc){
+						exc.printStackTrace();
+					}
+						
+						
+				}
+
+			}
+		});
+		btnSubmit.setBounds(319, 208, 117, 29);
+		contentPane.add(btnSubmit);
+		
+		username_field = new JTextField();
+		username_field.setBounds(150, 191, 130, 37);
+		contentPane.add(username_field);
+		username_field.setColumns(10);
+		
+		password_field = new JTextField();
+		password_field.setBounds(150, 235, 130, 37);
+		contentPane.add(password_field);
+		password_field.setColumns(10);
+		
+		JLabel lblUserName = new JLabel("User name");
+		lblUserName.setBounds(29, 201, 75, 16);
+		contentPane.add(lblUserName);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(29, 245, 61, 16);
+		contentPane.add(lblPassword);
+	}
+}
---
+++ src/UserLogin.java
@@ -0,0 +1,137 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.*;
+@SuppressWarnings("serial")
+public class UserLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField username_field;
+	private JTextField password_field;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UserLogin frame = new UserLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UserLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		username_field = new JTextField();
+		username_field.setBounds(209, 73, 158, 32);
+		contentPane.add(username_field);
+		username_field.setColumns(10);
+		
+		password_field = new JTextField();
+		password_field.setBounds(209, 166, 158, 32);
+		contentPane.add(password_field);
+		password_field.setColumns(10);
+		
+		JLabel lblUserName = new JLabel("User name:");
+		lblUserName.setBounds(81, 81, 79, 16);
+		contentPane.add(lblUserName);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(81, 174, 79, 16);
+		contentPane.add(lblPassword);
+		
+		JButton btnPatientLogin = new JButton("Patient Login");
+		btnPatientLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try{
+					Connection myConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Account", "root", "root");
+					String name = username_field.getText();
+					String password = password_field.getText();
+					String query = "select * from PatientId where user_name=? and password=?";
+					PreparedStatement statement = myConn.prepareStatement(query);
+					statement.setString(1, name);
+                    statement.setString(2, password);
+                    ResultSet set = statement.executeQuery();
+                    if(set.next()){
+                    	JOptionPane.showMessageDialog(null, "Login successfully");
+                    	UserInterface ui = new UserInterface();	                
+                    	ui.setVisible(true);
+                    	dispose();
+                    	
+                    }else{
+                    	JOptionPane.showMessageDialog(null, "Invalid password or user name..");
+                    }
+				}catch(Exception exc){
+					exc.printStackTrace();
+				}
+			}
+		});
+		btnPatientLogin.setBounds(40, 225, 117, 29);
+		contentPane.add(btnPatientLogin);
+		
+		JButton btnDoctorLogin = new JButton("Doctor Login");
+		btnDoctorLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try{
+					Connection myConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Account", "root", "root");
+					String name = username_field.getText();
+					String password = password_field.getText();
+					String query = "select * from DoctorIdInfo where username=? and password=?";
+					PreparedStatement statement = myConn.prepareStatement(query);
+					statement.setString(1, name);
+                    statement.setString(2, password);
+                    ResultSet set = statement.executeQuery();
+                    if(set.next()){
+                    	JOptionPane.showMessageDialog(null, "Login successfully");
+                    	DoctorInterface di = new DoctorInterface();	                
+                    	di.setVisible(true);
+                    	dispose();
+                    	
+                    }else{
+                    	JOptionPane.showMessageDialog(null, "Invalid password or user name..");
+                    }
+				}catch(Exception exc){
+					exc.printStackTrace();
+				}
+
+			}
+		});
+		btnDoctorLogin.setBounds(185, 225, 117, 29);
+		contentPane.add(btnDoctorLogin);
+		
+		JButton btnRegister = new JButton("Register");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegisterPage rp = new RegisterPage();
+				rp.setVisible(true);
+				dispose();
+			}
+		});
+		btnRegister.setBounds(314, 225, 117, 29);
+		contentPane.add(btnRegister);
+		
+	}
+}
